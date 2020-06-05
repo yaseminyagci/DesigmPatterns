d024441cc371db3a729001b7c72592a8f4f69be9
Yasemin <y.yagci@metacortex.com.tr>
2020-06-05 15:39:48 +0300

    Singleton pattern

---
+++ SingletonPattern/Singleton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public class Singleton
+    {
+        private static Singleton instance;
+
+        private Singleton()
+        {
+
+        }
+        public static Singleton getInstance()
+        {
+
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+
+        }
+    }
+}
+/*
+    Singleton Design Pattern
+    Burada ki amaç bir sınıftan bir instance oluşturmak ve yeni oluşturulacaksa var olanın 
+    kulanılmasını sağlar.
+    Bu tasarım deseninde constructer private olmalıdır. Bu yapılan işlem new
+    ile nesne oluşturmasını engeller.
+    class il aynı türde static bir mamber oluşturulur.
+    static membera ulaşmak için ise static bir metod yazılır. 
+     
+*/
